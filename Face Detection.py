e3813ff736fdcf8378497e9b98162e65f9e2fc15
Yash Dattatraya Pawar <71833743+yashpawar394@users.noreply.github.com>
2021-05-28 14:18:46 +0530

    Add files via upload

---
+++ Face Detection.py
@@ -0,0 +1,29 @@
+import cv2
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1280)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH,720)
+
+
+fincas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+#img = cv2.imread("1544629383911.jpg")
+
+while True:
+    _, frame = cap.read()
+    
+    faces = fincas.detectMultiScale(frame, 1.1, 4)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    cv2.imshow("test1", frame)
+    
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+cap.release()
+cv2.destroyAllWindows()
